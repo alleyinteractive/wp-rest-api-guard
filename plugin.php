e44136521bbea78575cb44278a4a5410c855c87b
Sean Fisher <srtfisher@gmail.com>
2022-10-05 17:35:37 -0400

    Adding the base plugin filters

--- plugin.php
+++ plugin.php
@@ -17,36 +17,121 @@
 
 namespace Alley\WP\REST_API_Guard;
 
+use WP_Error;
+use WP_REST_Request;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
-// Check if Composer is installed.
-if ( ! file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-	\add_action(
-		'admin_notices',
-		function() {
-			?>
-			<div class="notice notice-error">
-				<p><?php esc_html_e( 'Composer is not installed and rest-api-guard cannot load. Try using a `*-built` branch if the plugin is being loaded as a submodule.', 'plugin_domain' ); ?></p>
-			</div>
-			<?php
-		}
-	);
-
-	return;
-}
-
-// Load Composer dependencies.
-require_once __DIR__ . '/vendor/autoload.php';
-
-// Load the plugin's main files.
-require_once __DIR__ . '/src/meta.php';
 
 /**
  * Instantiate the plugin.
  */
 function main() {
-	// ...
+	add_action( 'rest_pre_dispatch', __NAMESPACE__ . '\on_rest_pre_dispatch', 10, 3 );
 }
 main();
+
+/**
+ * Check if anonymous access should be prevented for the current request.
+ *
+ * @param WP_REST_Request $request The request object.
+ * @return bool
+ */
+function should_prevent_anonymous_access( WP_REST_Request $request ): bool {
+	/**
+	 * Check if anonymous access is prevent by default.
+	 *
+	 * @param bool             $prevent Whether to prevent anonymous access, default false.
+	 * @param \WP_REST_Request $request REST API Request.
+	 */
+	if ( true === apply_filters( 'rest_api_guard_prevent_anonymous_access', false, $request ) ) {
+		return true;
+	}
+
+	$endpoint = $request->get_route();
+
+	/**
+	 * Prevent access to the /wp/v2/users endpoints by default.
+	 *
+	 * @param bool $pre Whether to prevent access to the /wp/v2/users endpoints.
+	 */
+	if ( preg_match( '#^/wp/v\d+/users($|/)#', $endpoint ) && false === apply_filters( 'rest_api_guard_allow_user_access', false ) ) {
+		return true;
+	}
+
+	// todo: check settings.
+
+	/**
+	 * Filter the allowlist for allowed anonymous requests.
+	 *
+	 * @param string[]         $allowlist Allowlist of requests.
+	 * @param \WP_REST_Request $request   REST API Request.
+	 */
+	$allowlist = apply_filters( 'rest_api_guard_anonymous_requests_allowlist', [] );
+
+	if ( is_array( $allowlist ) && ! empty( $allowlist ) ) {
+
+		foreach ( $allowlist as $allowlist_endpoint ) {
+			if ( preg_match( '/' . str_replace( '\*', '.*', preg_quote( $allowlist_endpoint, '/' ) ) . '/', $endpoint ) ) {
+				return false;
+			}
+		}
+
+		// If no route on the allowlist was matched, prevent anonymous access.
+		return true;
+	}
+
+	/**
+	 * Filter the denylist for allowed anonymous requests.
+	 *
+	 * @param string[]         $denylist Denylist of requests.
+	 * @param \WP_REST_Request $request  REST API Request.
+	 */
+	$denylist = apply_filters( 'rest_api_guard_anonymous_requests_denylist', [] );
+
+	if ( is_array( $denylist ) && ! empty( $denylist ) ) {
+		foreach ( $denylist as $denylist_endpoint ) {
+			if ( preg_match( '/' . str_replace( '\*', '.*', preg_quote( $denylist_endpoint, '/' ) ) . '/', $endpoint ) ) {
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+/**
+ * Short-circuit the REST API request if the user is not allowed to access it.
+ *
+ * @param  mixed            $pre     Dispatched value. Will be used if not empty.
+ * @param \WP_REST_Server  $server  Server instance.
+ * @param \WP_REST_Request $request REST API Request.
+ * @return mixed
+ */
+function on_rest_pre_dispatch( $pre, $server, $request ) {
+	if ( ! empty( $pre ) || is_user_logged_in() ) {
+		return $pre;
+	}
+
+	// Check if anonymous access is prevent by default.
+	if ( should_prevent_anonymous_access( $request ) ) {
+		return new WP_Error(
+			'rest_api_guard_unauthorized',
+			/**
+			 * Filter the authorization error message.
+			 *
+			 * @param string $message The error message.
+			 */
+			apply_filters(
+				'rest_api_guard_unauthorized_message',
+				__( 'Sorry, you are not allowed to access this request.', 'rest-api-guard' ),
+			),
+			[
+				'status' => rest_authorization_required_code(),
+			]
+		);
+	}
+
+	return $pre;
+}
---
+++ tests/test-rest-api-guard.php
@@ -0,0 +1,75 @@
+<?php
+namespace Alley\WP\REST_API_Guard\Tests;
+
+/**
+ * Visit {@see https://mantle.alley.co/testing/test-framework.html} to learn more.
+ */
+class Test_REST_API_Guard extends Test_Case {
+	public function test_default_anonymous_access() {
+		$this->get( rest_url( '/wp/v2' ) )->assertOk();
+		$this->get( rest_url( '/wp/v2/posts' ) )->assertOk();
+		$this->get( rest_url( '/wp/v2/tags' ) )->assertOk();
+
+		// By default, users are not allowed.
+		$this->get( rest_url( '/wp/v2/users' ) )->assertUnauthorized();
+
+		$this->acting_as( 'administrator' );
+
+		$this->get( rest_url( '/wp/v2/users' ) )->assertOk();
+	}
+
+	public function test_allow_user_access() {
+		add_filter( 'rest_api_guard_allow_user_access', fn () => true );
+
+		$this->get( rest_url( '/wp/v2/users' ) )->assertOk();
+	}
+
+	// public function test_prevent_anonymous_access_settings() {}
+
+	public function test_prevent_anonymous_access_code() {
+		add_filter( 'rest_api_guard_prevent_anonymous_access', fn () => true );
+
+		$this->get( rest_url( '/wp/v2' ) )->assertUnauthorized();
+		$this->get( rest_url( '/wp/v2/posts' ) )->assertUnauthorized();
+		$this->get( rest_url( '/wp/v2/tags' ) )->assertUnauthorized();
+	}
+
+	// public function test_prevent_access_allowlist_setting() {}
+
+	public function test_prevent_access_allowlist_code() {
+		add_filter(
+			'rest_api_guard_anonymous_requests_allowlist',
+			fn () => [
+				'/wp/v2/posts/*',
+				'/wp/v2/tags',
+			]
+		);
+
+		$post_id = static::factory()->post->create();
+
+		$this->get( rest_url( '/wp/v2' ) )->assertUnauthorized();
+		$this->get( rest_url( '/wp/v2/posts' ) )->assertUnauthorized();
+		$this->get( rest_url( '/wp/v2/posts/' . $post_id ) )->assertOk();
+		$this->get( rest_url( '/wp/v2/tags' ) )->assertOk();
+	}
+
+	// public function test_prevent_access_denylist_setting() {}
+
+	public function test_prevent_access_denylist_code() {
+		add_filter(
+			'rest_api_guard_anonymous_requests_denylist',
+			fn () => [
+				'/wp/v2/tags',
+				'/wp/v2/types',
+			]
+		);
+
+		$this->get( rest_url( '/wp/v2' ) )->assertOk();
+		$this->get( rest_url( '/wp/v2/posts' ) )->assertOk();
+		$this->get( rest_url( '/wp/v2/posts/' . static::factory()->post->create() ) )->assertOk();
+		$this->get( rest_url( '/wp/v2/tags' ) )->assertUnauthorized();
+		$this->get( rest_url( '/wp/v2/types' ) )->assertUnauthorized();
+	}
+
+	// public function test_prevent_access_denylist_priority() {}
+}
