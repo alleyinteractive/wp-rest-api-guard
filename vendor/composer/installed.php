d82ed41cef24c4a626c3973167bc8975125b02f9
srtfisher <srtfisher@users.noreply.github.com>
2026-09-19 00:55:58 +0000

    Built changes for v1.4.2 from develop 1738db5ec7f1be93ec4f6fe2d7a20a3b4ef0e537

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alleyinteractive/wp-rest-api-guard',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '1738db5ec7f1be93ec4f6fe2d7a20a3b4ef0e537',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'alleyinteractive/wp-rest-api-guard' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '1738db5ec7f1be93ec4f6fe2d7a20a3b4ef0e537',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v7.1.1',
+            'version' => '7.1.1.0',
+            'reference' => '9bc93bd7e3ee5bead4cd23c365ec12f3c1fb0a6a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
